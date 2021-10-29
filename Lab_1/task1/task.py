d3cca4a84b3d314611342d7604739df796d5e842
Valeriya Artamonova <lerartlera@gmail.com>
2021-10-30 02:13:20 +0300

    1, 2, 3 задачи в лабе 2

--- Lab_1/task1/task.py
+++ Lab_1/task1/task.py
@@ -7,15 +7,36 @@ tracemalloc.start()
 # чтение данных из файла
 
 input_file = open('input.txt', 'r')
-
+n, numbers = input_file.readlines()
+n = int(n)
+numbers = list(map(int, numbers.split(',')))
 input_file.close()
 # основная функция
 
+
+def insertion_sort(n, numbers):
+    """
+    Функция сортировки вставкой по возрастанию
+    :param n: количество чисел в массиве
+    :param numbers: массив чисел
+    :return: отсортированный массив
+    """
+    for j in range(1, n):
+        key = numbers[j]
+        i = j - 1
+        while i >= 0 and numbers[i] >= key:
+            numbers[i + 1] = numbers[i]
+            i = i - 1
+        numbers[i + 1] = key
+    return numbers
 # вызов функции
+
+sorted_numbers = insertion_sort(n, numbers)
 
 # запись результата в файл
 output_file = open('output.txt', 'w')
-
+sorted_numbers_str = list(map(str, sorted_numbers))
+output_file.write(' '.join(sorted_numbers_str))
 output_file.close()
 
 # вывод времени работы алгоритма и затраченной памяти
---
+++ Lab_1/task2/task.py
@@ -0,0 +1,52 @@
+# загрузка библиотек
+import time
+import tracemalloc
+
+t_start = time.perf_counter()
+tracemalloc.start()
+# чтение данных из файла
+
+input_file = open('input.txt', 'r')
+n, numbers = input_file.readlines()
+n = int(n)
+numbers = list(map(int, numbers.split(' ')))
+input_file.close()
+# основная функция
+
+
+def insertion_sort(n, numbers):
+    """
+    Функция сортировки вставкой по возрастанию
+    :param n: количество чисел в массиве
+    :param numbers: массив чисел
+    :return: массив индексов и отсортированный массив
+    """
+    indexes = []
+    indexes.append(1)
+    for j in range(1, n):
+        key = numbers[j]
+        i = j - 1
+        while i >= 0 and numbers[i] >= key:
+            numbers[i + 1] = numbers[i]
+            i = i - 1
+        numbers[i + 1] = key
+        indexes.append(i+2)
+    return indexes, numbers
+# вызов функции
+
+indexes, sorted_numbers = insertion_sort(n, numbers)
+
+# запись результата в файл
+output_file = open('output.txt', 'w')
+sorted_numbers_str = list(map(str, sorted_numbers))
+indexes = list(map(str, indexes))
+output_file.write(' '.join(indexes))
+output_file.write('\n')
+output_file.write(' '.join(sorted_numbers_str))
+output_file.close()
+
+# вывод времени работы алгоритма и затраченной памяти
+
+print("Время работы алгоритма ", (time.perf_counter() - t_start), "секунд")
+print("Текущий объем затрачиваемой памяти на работу алгоритма ", tracemalloc.get_traced_memory()[0]/2**20, "MB")
+print("Пиковый объем затрачиваемой памяти на работу алгоритма ", tracemalloc.get_traced_memory()[1]/2**20, "MB")
